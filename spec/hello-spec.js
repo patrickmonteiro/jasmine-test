fbd6f02eaa10149409b03dd423755d07ffa77aec
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2020-05-05 22:23:20 -0300

    First commit :tada:

---
+++ spec/hello-spec.js
@@ -0,0 +1,8 @@
+var hello = require('../hello');
+
+describe('Hello', function(){
+  it('has to print "Hello World"', function(){
+    var text = hello();
+    expect(text).toEqual('Hello World!');
+  })
+})
